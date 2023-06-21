5bf84dcd6515a8bf34f2f09484e1ea046c590df9
chandiosamad07 <absamadchandio@gmail.com>
2023-06-22 01:12:07 +0500

    DOne with Backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
+const cors = require('cors')
+const routes = require('./routes/ToDoRoute')
+
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/Todo', {
@@ -14,7 +17,10 @@ mongoose.connect('mongodb://localhost/Todo', {
     console.error('Error connecting to MongoDB:', error);
   });
 
+  app.use(routes)
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(5000, () => console.log('Example app listening on port 3000!'))
+app.use(express.json())
+app.use(cors())
