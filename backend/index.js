3aaf25864f96e14d5300bdc89dad6104f1d54fd9
chandiosamad07 <absamadchandio@gmail.com>
2023-06-22 00:02:48 +0500

    testing MongoDB and Express

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/Todo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(5000, () => console.log('Example app listening on port 3000!'))
